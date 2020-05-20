301fbfaa8b28d1df67c0f428eb9cdb30aeb51d14
FoxMD <ryusei@seznam.cz>
2020-05-21 00:22:02 +0200

    Test server

--- myServer.py
+++ myServer.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 print("Starting script:" + __name__)
 app = Flask(__name__)
@@ -6,9 +6,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "hello world"
+    items = ["Item 1", "Item 2", "Item 3"]
+
+    return render_template("start.html", head="myHeader", items=items)
 
 
 @app.route("/test")
 def test():
-    return "hello world this is test"
+    args = request.args
+    name = args.get("name")
+    print(name)
+    return render_template("site1.html", param1=name)
