d8f1fa25ced329e3084f946c79a6805804848905
FoxMD <ryusei@seznam.cz>
2020-05-20 15:11:56 +0200

    New site test

--- myServer.py
+++ myServer.py
@@ -1,6 +1,14 @@
 from flask import Flask
+
+print("Starting script:" + __name__)
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "hello world"
+
+
+@app.route("/test")
+def test():
+    return "hello world this is test"
